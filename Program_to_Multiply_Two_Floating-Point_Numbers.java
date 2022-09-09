4e6183dcdea35ab7861f0dc3c87320b80715b7fd
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-09-10 01:14:21 +0530

    import java.util.Scanner; class ArithmeticOperations {     public static void main(String args[])     {         float a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextFloat();         b=sc.nextFloat();
            System.out.format("%.2f",a*b);
            
            sc.close();
        }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class ArithmeticOperations
+{
+    public static void main(String args[])
+    {
+        float a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        
+        System.out.format("%.2f",a*b);
+        
+        sc.close();
+    }
+}
