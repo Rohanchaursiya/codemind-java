2beaf1151efc0c3567a6a5449f803d10a9edbf66
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-09-09 10:33:34 +0530

    import java.util.Scanner; class ArithmeticOperations {     public static void main(String args[])     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();
            System.out.println(a/b);
            
            sc.close();
        }
    }

---
+++ Division.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class ArithmeticOperations
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        
+        System.out.println(a/b);
+        
+        sc.close();
+    }
+}
