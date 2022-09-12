86bc59369aae9c3c77860d046ac13c3366f40e52
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-09-13 01:38:23 +0530

    import java.util.Scanner; class CanRobberEscape {     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[100];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         for(int i=n-1;i>=0;i--){             if(arr[i]%2==0){                 System.out.print(i);                 break;             }         }     sc.close();     } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class CanRobberEscape
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[100];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]%2==0){
+                System.out.print(i);
+                break;
+            }
+        }
+    sc.close();
+    }
+}
