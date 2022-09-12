2d10c7e7aceb5008ddffe1aaeee3a437727581c5
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-09-13 01:47:39 +0530

    import java.util.Scanner; class ElementSearch {     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[10000];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         int k,count=0;         k=sc.nextInt();         for(int i=0;i<n;i++){             if(arr[i]==k){                 count+=1;             }         }         if(count!=0){             System.out.print("True");         }         else{             System.out.print("False");         }     sc.close();     } }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class ElementSearch
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[10000];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int k,count=0;
+        k=sc.nextInt();
+        for(int i=0;i<n;i++){
+            if(arr[i]==k){
+                count+=1;
+            }
+        }
+        if(count!=0){
+            System.out.print("True");
+        }
+        else{
+            System.out.print("False");
+        }
+    sc.close();
+    }
+}
