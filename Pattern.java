d72acb9ab90de165cac9cef0d76ad4e713beaa62
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-09-15 21:57:55 +0530

    import java.util.Scanner; class Patttern{     public static void main(String args[]){         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         for(int i=1;i<=n;i++){             for(int j=1;j<=n;j++){                 // if(i==j || i+j==n+1){                 //     System.out.print("x");                 // }                 // else{                     System.out.print(i+" ");                 //}             }             System.out.println();         }     } }

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Patttern{
+    public static void main(String args[]){
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                // if(i==j || i+j==n+1){
+                //     System.out.print("x");
+                // }
+                // else{
+                    System.out.print(i+" ");
+                //}
+            }
+            System.out.println();
+        }
+    }
+}
