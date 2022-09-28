907624bdf18628c6032ab517df31fb41aa366374
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-09-28 22:40:28 +0530

    import java.util.*; class Sum{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int m,n;         m=sc.nextInt();         n=sc.nextInt();         int a[][]=new int[10][10];         for(int i=0;i<m;i++){             for(int j=0;j<n;j++){                 a[i][j]=sc.nextInt();             }         }         int sum=0;         for(int i=0;i<m;i++){             for(int j=0;j<n;j++){                 if(i==0 || i==m-1 || j==0 || j==n-1){                     sum+=a[i][j];                 }             }         }         System.out.print(sum);     } }

---
+++ Sum_of_all_boundary_values_in_a_matrix.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Sum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int m,n;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        int a[][]=new int[10][10];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        int sum=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || i==m-1 || j==0 || j==n-1){
+                    sum+=a[i][j];
+                }
+            }
+        }
+        System.out.print(sum);
+    }
+}
