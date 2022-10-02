fb156889eadfe99190ce280b1ad0466269f64d01
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-10-02 11:47:49 +0530

    import java.util.*; class Pattern{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=n;i>=1;i--){             for(int j=1;j<i;j++){                 System.out.print(" ");             }             for(int k=1;k<=n;k++){                 if(i==1||i==n||k==1||k==n){                     System.out.print("*");                 }                 else{                     System.out.print(" ");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,15 +1,22 @@
 import java.util.*;
-class pattern{
+class Pattern{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n;
         n=sc.nextInt();
         for(int i=n;i>=1;i--){
-            for(int k=1;k<=i;k++){
-                System.out.print(k);
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
             }
-            System.out.print("
-");
+            for(int k=1;k<=n;k++){
+                if(i==1||i==n||k==1||k==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
     }
 }
