bd2bb006c107ddd0c1dc2026dc6489fe18871554
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-10-02 12:10:50 +0530

    import java.util.Scanner; class Hello_T_Hub{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();     	System.out.println("Hello Technicalhub");     	System.out.println(s1);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Hello_T_Hub{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+    	System.out.println("Hello Technicalhub");
+    	System.out.println(s1);
+    }
+}
