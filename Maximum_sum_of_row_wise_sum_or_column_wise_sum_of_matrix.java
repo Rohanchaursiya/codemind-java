464f12d88b0aaa763bbcc74e5ab49aedacf42e54
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-12-10 00:16:05 +0530

    import java.util.*; class Matrix{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int m=sc.nextInt();         int n=sc.nextInt();         int[][] arr=new int[m][n];         int temp=0;         for(int i=0;i<m;i++){             int sum=0;             for(int j=0;j<n;j++){                 arr[i][j]=sc.nextInt();                 sum+=arr[i][j];             }             if(sum>temp){                 temp=sum;             }         }         for(int j=0;j<n;j++){             int sum=0;             for(int i=0;i<m;i++){                 sum+=arr[i][j];             }             if(sum>temp){                 temp=sum;             }         }         System.out.println(temp);     } }

---
+++ Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Matrix{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int[][] arr=new int[m][n];
+        int temp=0;
+        for(int i=0;i<m;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+                sum+=arr[i][j];
+            }
+            if(sum>temp){
+                temp=sum;
+            }
+        }
+        for(int j=0;j<n;j++){
+            int sum=0;
+            for(int i=0;i<m;i++){
+                sum+=arr[i][j];
+            }
+            if(sum>temp){
+                temp=sum;
+            }
+        }
+        System.out.println(temp);
+    }
+}
