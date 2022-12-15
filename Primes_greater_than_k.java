0bc05c5dfff05fee2f7a815b9dd0fdc58538c40c
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-12-15 14:10:14 +0530

    import java.util.*; class Prime{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++){             a[i]=sc.nextInt();         }         int k=sc.nextInt();         int count=0,c=0;         for(int i=0;i<n;i++){             c=0;             if(a[i]>1){                 for(int j=2;j<=Math.sqrt(a[i]);j++){                     if(a[i]%j==0){                         c=1;                         break;                     }                 }                 if(c==0){                     if(a[i]>=k){                         count++;                     }                 }             }         }         System.out.print(count);     } }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Prime{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        int count=0,c=0;
+        for(int i=0;i<n;i++){
+            c=0;
+            if(a[i]>1){
+                for(int j=2;j<=Math.sqrt(a[i]);j++){
+                    if(a[i]%j==0){
+                        c=1;
+                        break;
+                    }
+                }
+                if(c==0){
+                    if(a[i]>=k){
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.print(count);
+    }
+}
