f299aa5800e53913ed9811cb35314e97bf62370f
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2023-05-29 11:38:46 +0530

    import java.util.*; public class String_Multiply{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String s1=sc.next();         String s2=sc.next();         long a=Long.parseLong(s1);         long b=Long.parseLong(s2);
            System.out.println(a*b);
        }
    }

---
+++ Multiply_Strings.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class String_Multiply{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.next();
+        String s2=sc.next();
+        long a=Long.parseLong(s1);
+        long b=Long.parseLong(s2);
+        
+        System.out.println(a*b);
+    }
+}
